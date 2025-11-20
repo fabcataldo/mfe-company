4f040acb7e73b4da825b154d71d20dcd7bf7e6b7
Fabio <fabioalbertocataldo@gmail.com>
2025-11-20 14:00:35 -0300

    created container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
@@ -23,9 +23,6 @@ const devConfig = {
             //I want webpack to manage automatically my dependencies
             shared: packageJson.dependencies
         }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
     ]
 };
 
